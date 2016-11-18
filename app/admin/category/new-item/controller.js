cfec980b87c07cb488b3075d72d0b43f8914633f
“Shaun <shaunwmusic@gmail.com>
2016-11-18 09:56:28 -0600

    Dynamic URL for admin/category/detail

--- app/admin/category/new-item/controller.js
+++ app/admin/category/new-item/controller.js
@@ -1,21 +1,17 @@
 import Ember from 'ember';
 
-
 export default Ember.Controller.extend({
-  actions: {
-    saveForm(name, goal, description) {
-      if (name && goal && description) {
-        const menu = this.store.createRecord('menu-item', { name, goal, description });
+    actions: {
+      submit(formValues) {
+        const newItem = this.store.createRecord('menu-item', formValues);
 
-        menu.save()
-          .then(() => {
-            alert('Menu saved!');
+        newItem.set('menuCategory', this.model);
 
-            this.transitionToRoute('category.index');
-          });
-      } else {
-        alert('You must fill in the form');
-      }
+        newItem.save()
+        .then(() => {
+          this.transitionToRoute('admin.category.detail', this.model.id);
+        });
+
+      },
     },
-  },
 });
